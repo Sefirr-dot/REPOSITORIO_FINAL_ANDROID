e6b08b5d71868a720e9a5249f503463969046998
Miguel-León <159438587+Sefirr-dot@users.noreply.github.com>
2024-09-26 19:23:27 +0200

    Segundoo

---
+++ ControlesConFuncionalidad/app/src/main/java/com/example/controlesconfuncionalidad/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.controlesconfuncionalidad
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val miTag = "Antonio"
+        val ptNombre = findViewById<EditText>(R.id.ptNombre)
+        val btAceptar = findViewById<Button>(R.id.btAceptar)
+        val btBorrar = findViewById<Button>(R.id.btBorrar)
+
+        btAceptar.setOnClickListener {
+
+            Log.i(miTag, "Hola ${ptNombre.text}")
+            Toast.makeText(this, "Hola ${ptNombre.text}", Toast.LENGTH_SHORT).show()
+        }
+        btBorrar.setOnClickListener {
+            ptNombre.text.clear()
+        }
+
+    }
+}
