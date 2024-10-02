76f90aa3d31c266f0a21619ff466b40760dc6d79
Miguel-León <159438587+Sefirr-dot@users.noreply.github.com>
2024-10-02 21:11:54 +0200

    CALCULADORA ANDROID

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.calculadora
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,7 +22,42 @@ class MainActivity : AppCompatActivity() {
         }
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btSuma.setOnClickListener { suma() }
+        binding.btResta.setOnClickListener { resta() }
+        binding.btMultiplicacion.setOnClickListener { multiplicacion() }
+        binding.btDivision.setOnClickListener {
+            if(binding.etxtn2.text.toString().toDouble() == 0.0 || binding.etxtn1.text.toString().isEmpty() || binding.etxtn2.text.toString().isEmpty()){
+                Toast.makeText(this, "No se puede dividir entre 0", Toast.LENGTH_SHORT).show()
+            } else{
+                division()
+            }
+             }
 
-        
+
+    }
+    fun suma(){
+        val num1 = binding.etxtn1.text.toString().toDouble()
+        val num2 = binding.etxtn2.text.toString().toDouble()
+        val resultado = num1 + num2
+        binding.txtResultado.text = resultado.toString()
+    }
+    fun resta(){
+        val num1 = binding.etxtn1.text.toString().toDouble()
+        val num2 = binding.etxtn2.text.toString().toDouble()
+        val resultado = num1 - num2
+        binding.txtResultado.text = resultado.toString()
+    }
+    fun multiplicacion(){
+        val num1 = binding.etxtn1.text.toString().toDouble()
+        val num2 = binding.etxtn2.text.toString().toDouble()
+        val resultado = num1 * num2
+        binding.txtResultado.text = resultado.toString()
+    }
+    fun division(){
+        val num1 = binding.etxtn1.text.toString().toDouble()
+        val num2 = binding.etxtn2.text.toString().toDouble()
+        val resultado = num1 / num2
+        binding.txtResultado.text = resultado.toString()
+        binding.txtModuloDivision.text = (num1 % num2).toString()
     }
 }
